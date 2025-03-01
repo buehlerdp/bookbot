c2594cf1ca407979e0122f6a414378c135042340
buehlerdp <buehlerdp@gmail.com>
2025-03-01 10:12:51 -0600

    Orderd list of letters only

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from stats import word_count
 
 from stats import character_count
 
+from stats import make_list
+
 def get_book_text(book_path):
     with open (book_path) as f:
         file_contents = f.read() 
@@ -11,8 +13,13 @@ def get_book_text(book_path):
 
 def main():
     tocount = get_book_text("./books/frankenstein.txt")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at /books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     word_count (tocount)
-    character_count (tocount)
-    
+    print("--------- Character Count -------")
+    intermediate = character_count (tocount)
+    make_list (intermediate)
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -1,19 +1,31 @@
 def word_count(text):
     words = text.split()
     length = len (words)
-    #return (length)
-    print (f"{length} words found in the document")
+    print (f"Found {length} total words")
 
 
 def character_count(text):
     lowercase = text.lower()
-    counts = {}
-    #"a": lowercase.count("a")  
+    counts = {} 
     for char in lowercase:
-        if char in counts:
+        if (char in counts) and (char.isalpha() == True):
             counts[char] += 1
-        else:
+        elif (char.isalpha() == True):
             counts[char] = 1
-    print (counts)
+    return (counts)
 
- 
+
+#def sort_on(tosort):
+  #  return tosort["num"]
+
+
+def make_list(dict):
+    listofdict = [{key:value} for key, value in dict.items()]
+    sortedlist = sorted(listofdict, key=lambda d: list(d.values())[0], reverse=True)
+    for entry in sortedlist:
+        for key, value in entry.items():
+            print (f"{key}: {value}")
+
+    
+   
+    
